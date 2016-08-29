b77a1a62b35a3b30834263446ec7ac6b5d10b933
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2016-08-29 17:50:13 +0200

    Subsampling, partial rewrite

--- subsample/scripts/run.sh
+++ subsample/scripts/run.sh
@@ -1,39 +1,73 @@
-#!/bin/bash
+#!/bin/bash -
 
-THREADS=8
+## Print a header
+SCRIPT_NAME="subsampling comparison"
+LINE=$(printf "%076s\n" | tr " " "-")
+printf "# %s %s\n" "${LINE:${#SCRIPT_NAME}}" "${SCRIPT_NAME}"
 
-mkdir -p results
+failure () {
+    printf "FAIL: ${1}\n"
+    exit -1
+}
 
-BASE=TARA_V9_264_samples
-#BASE=BioMarKs
-DATAGZ=data/$BASE.fsa.gz
-DATA=data/$BASE.fsa
-RES=results/$BASE.subsample.tsv
-REPLICATES=10000
+## Check for dependencies (usearch, vsearch)
+VSEARCH=$(which vsearch)
+DESCRIPTION="vsearch binary not found"
+[[ "${VSEARCH}" ]] || failure "${DESCRIPTION}"
+USEARCH8=$(which usearch8)
+DESCRIPTION="usearch binary not found"
+[[ "${USEARCH8}" ]] || failure "${DESCRIPTION}"
 
-if [ ! -e $DATA ]; then
-    gunzip -c -d $DATAGZ > $DATA
-fi
+## Get data (check first)
+DATA="../data"
+mkdir -p "${DATA}"
+FASTA="TARA_V9_264_samples.fas"
+URL="https://elwe.rhrk.uni-kl.de/outgoing/${FASTA}"
+[[ -s "${DATA}/${FASTA}" ]] || (cd ${DATA}/ ; wget "${URL}")
 
-if [ ! -e $RES ]; then
+## Subsample once to avoid usearch memory limit
+INITIAL_PERCENTAGE="10.0"
+INPUT=$(mktemp)
+${VSEARCH} \
+    --fastx_subsample "${DATA}/${FASTA}" \
+    --fastaout "${INPUT}" \
+    --randseed 1 \
+    --sample_pct "${INITIAL_PERCENTAGE}" \
+    --sizein \
+    --sizeout > /dev/null
+NOMINAL_SIZE=$(head -n 1 "${INPUT}" | cut -d "=" -f 2  | tr -d ";")
 
-    echo -e "fraction\tprogram\tabundance" > $RES
+## Subsample 10,000 times with vsearch and usearch
+TMP_FASTA=$(mktemp)
+for PERCENTAGE in 1 5 10 20 50 ; do
+    LOG="${TARA/.fas/.subsample}_${PERCENTAGE/.0/_head}.log"
+    for i in {1..10000} ; do
+        ${VSEARCH} \
+            --fastx_subsample "${INPUT}" \
+            --fastaout "${TMP_FASTA}" \
+            --randseed "${i}" \
+            --sample_pct "${PERCENTAGE}" \
+            --sizein \
+            --sizeout > /dev/null
 
-    for f in 1 2 3 5; do
-        for n in $(seq 1 $REPLICATES); do
-            for p in usearch8 vsearch; do
+        SIZE_VSEARCH=$(head -n 1 "${TMP_FASTA}" | cut -d "=" -f 2 | tr -d ";")
 
-                $p --fastx_subsample $DATA \
-                    --sample_pct $f \
-                    --sizein \
-                    --sizeout \
-                    --threads $THREADS \
-                    --fastaout results/temp.fasta
-                t=$(head -1 results/temp.fasta | cut -d= -f2 | cut -d\; -f1)
-                echo -e "$f\t$p\t$t" >> $RES
-                rm results/temp.fasta
-            done
-        done
-    done
+        ${USEARCH8} \
+            -fastx_subsample "${INPUT}" \
+            -fastaout "${TMP_FASTA}" \
+            -randseed "${i}" \
+            -sample_pct "${PERCENTAGE}" \
+            -sizein \
+            -sizeout > /dev/null
 
-fi
+        SIZE_USEARCH8=$(head -n 1 "${TMP_FASTA}" | cut -d "=" -f 2 | tr -d ";")
+
+        echo -e "${PERCENTAGE}\t${NOMINAL_SIZE}\t${SEED}\t${SIZE_VSEARCH}\t${SIZE_USEARCH8}"
+    done >> ${LOG}
+done
+    
+rm "${TMP_FASTA}" "${INPUT}"
+
+exit 0
+
+## TODO produce plot (launch a R script, that R script is going to check for dependencies)
