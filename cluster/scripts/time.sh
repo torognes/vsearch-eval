e9cf480c00376c8f810469dbab6dac1959c6cad5
torognes <torognes@ifi.uio.no>
2016-09-01 00:14:58 +0200

    Fix timing

--- cluster/scripts/time.sh
+++ cluster/scripts/time.sh
@@ -5,8 +5,10 @@ for D in even uneven; do
         for P in usearch usearch8 vsearch; do
             for id in 80 90 95 96 97 98 99; do
                 for F in results/$D/$D.shuffle_*.fasta.$P.cluster_$A.$id.log; do
-                    N=$(grep "^real" $F | cut -c6-)
-                    echo -e "$D\t$A\t$P\t$id\t$N";
+                    if [ -e $F ] ; then
+                        N=$(grep "^real" $F | cut -c6-)
+                        echo -e "$D\t$A\t$P\t$id\t$N";
+                    fi
                 done
             done
         done
